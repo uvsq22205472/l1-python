cc53ca48b67b4e64e400b1336ca2ca2034e87db8
uvsq22205472 <oliwier.szmyt@envs.uvsq.fr>
2022-10-24 13:43:34 +0200

    maj

---
+++ exercises/OUI_SI_Morpion.py
@@ -0,0 +1,12 @@
+# Morpion
+
+# O X O 
+# X O O
+# X O X
+
+# Initiation des variables =-=-=-=
+ligneA = ('', '', '')
+ligneB = ('', '', '')
+ligneC = ('', '', '')
+
+
---
+++ exercises/QuestionsSupp.py
@@ -0,0 +1,22 @@
+import random
+
+
+# Q1
+#a = int(input("Entrez un nombre entier a > "))
+#b = int(input("Entrez un nombre entier b > "))
+#if a > b:
+#    a, b = b, a
+#print(a, b)
+
+# Q2
+#nombre_choisi = None
+#nombre_aleatoire = random.randint(10,99)
+#while len(str(nombre_choisi)) != 1 :
+#    nombre_choisi = int(input('Entrez un seul chiffre.'))
+#if nombre_aleatoire % nombre_choisi == 0 and nombre_choisi != 1:
+#    print(nombre_choisi,'est un diviseur de ',nombre_aleatoire)
+#else:
+#    print(nombre_choisi,"n'est pas un diviseur de",nombre_aleatoire)
+
+
+# Q3
